953a7bbc530c45ba83e898e6b1cc93c32aa56ada
clementcode <mrdominiqueclement@gmail.com>
2021-08-05 12:45:47 +0200

    gogogo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <h2>Voici mon CV </h2>
             <h3>Bienvenue sur mon CV qui est continuellement en chantier vous y trouverez une partie cv classique ainsi que differents liens :
                 -liens vers mon linkedin pour me contacter
-                -liens au bas de ma page pour aller sur mes differents projets (je ne les ai pas tous encore integrés)  les technologies sur lequel je me concentre et j'apprends encore tout les jours
+                -liens au bas de ma page pour aller sur mes differents projets (je ne les ai pas tous encore integrés)  les technologies sur lequel je me concentre et j'apprends encore tout les jours allday
                 
                 aussi si vous pou suivzez les liens tout a droite vous y trouverez une page contact pour me contacter directement par mail  directement bien sur
             </h3>
